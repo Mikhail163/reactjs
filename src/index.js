d5af5e48e29f111984ad255a107f8a6779aabbd0
Mikhail163 <mephist0_87@mail.ru>
2018-06-12 02:31:32 +0400

    task 1,2 complite task 3,4 50%

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import User from './user';
+import Developer from './user';
+import Manager from './user';
 
-const user = new User('Вася', 'Пупкин');
 
-user.sayHi();
+const manager = new Manager('Варфаламей', 'Плибеевич', 250000, 'отдел разработки');
+const dev1 = new Developer('Вася', 'Петров', 74000, 'отдел разработки', manager);
+const dev2 = new Developer('Петя', 'Иванов', 32000, 'отдел разработки', manager);
+const dev3 = new Developer('Михаил', 'Сидоров', 50000, 'отдел разработки', manager);
+
+
+dev1.render("developer");
+
+manager.render("manager");
+
 
 console.log('Hello world');
--- src/user.js
+++ src/user.js
@@ -1,6 +1,6 @@
 import './user.css';
 
-export default class User {
+class User {
     constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -9,4 +9,147 @@ export default class User {
     sayHi() {
         alert(`Hi, ${this.firstName} ${this.lastName}`);
     }
+
+    render(parentId) {
+        let content = document.getElementById(parentId);
+        content.innerHTML = this.getContent();
+    }
+
+    getContent() {
+        return `<div class="user">${this.firstName} ${this.lastName}</div>`;
+    }
+}
+
+
+class Employee extends User {
+
+    constructor(firstName, lastName, salary = 0, department = "") {
+
+        super(firstName, lastName);
+
+        this._salary = salary;
+        this._department = department;
+
+    }
+
+    displayInfo() {
+        super.displayInfo();
+        console.log(this.salary);
+        console.log(this.department);
+
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(newSalary) {
+        this._salary = newSalary;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    set department(newDepartment) {
+        this._department = newDepartment;
+    }
+
+    getContent() {
+        return `<div class="user">${this.firstName} ${this.lastName}: зп ${this._salary} р; отдел ${this._department};</div>`;
+    }
+
+}
+
+
+export default class Developer extends Employee {
+
+    constructor(firstName, lastName, salary = 0, department = "", manager = null) {
+        super(firstName, lastName, salary, department);
+
+        this._manager = null;
+        this.manager = manager;
+
+    }
+
+    get manager() {
+        return this._manager;
+    }
+
+    set manager(newManager) {
+
+        if (newManager != this._manager) {
+
+            if (newManager instanceof Manager || newManager === null) {
+
+                if (this._manager != null) {
+                    // если у рабочего есть manager
+                    // то у старого manager, удаляем этого сотрудника
+                    this._manager.deleteDeveloper(this);
+
+                }
+
+                // добавляем нового девелопера менеджеру
+                this._manager = newManager;
+
+                if (newManager != null) {
+                    this._manager.developer(this);
+                }
+            }
+
+
+        }
+
+    }
+}
+
+
+class Manager extends Employee {
+
+    constructor(firstName, lastName, salary = 0, department = "") {
+        console.log('create manager');
+        super(firstName, lastName, salary, department);
+
+        this._developers = [];
+        console.log('create manager');
+
+    }
+
+    set developer(newDeveloper) {
+        if (newDeveloper instanceof Developer && this._developers.indexOf(newDeveloper) === -1) {
+            this._developers.push(newDeveloper);
+
+            newDeveloper.manager = this;
+        }
+    }
+
+    get developers() {
+        return this._developers;
+    }
+
+    deleteDeveloper(developer) {
+
+        let index = this._developers.indexOf(developer);
+
+        if (index != -1) {
+            // Удаляем нашего сотрудника
+            this._developers.splice(index, 1);
+
+            // Теперь горемыка остался без менеджера((
+            developer.manager = null;
+        }
+    }
+
+    getContent() {
+
+        let content = `<div class="manager">${this.firstName} ${this.lastName}: зп ${this._salary} р; отдел ${this._department};`;
+
+        for (let i = 0; i < this._developers.length; i++) {
+            content += this._developers[i].getContent();
+        }
+
+        return content += `</div>`;
+
+    }
+
 }
