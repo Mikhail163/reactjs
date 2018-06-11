a735f047be542ab78c50fd2b91a430c238ddd06d
Mikhail163 <mephist0_87@mail.ru>
2018-06-12 03:31:20 +0400

    подкорректировали readme

--- src/user.js
+++ src/user.js
@@ -1,4 +1,4 @@
-class User {
+export default class User {
     constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -16,45 +16,4 @@ class User {
     getContent() {
         return `<div class="user">${this.firstName} ${this.lastName}</div>`;
     }
-}
-
-
-export default class Employee extends User {
-
-    constructor(firstName, lastName, salary = 0, department = "") {
-
-        super(firstName, lastName);
-
-        this._salary = salary;
-        this._department = department;
-
-    }
-
-    displayInfo() {
-        super.displayInfo();
-        console.log(this.salary);
-        console.log(this.department);
-
-    }
-
-    get salary() {
-        return this._salary;
-    }
-
-    set salary(newSalary) {
-        this._salary = newSalary;
-    }
-
-    get department() {
-        return this._department;
-    }
-
-    set department(newDepartment) {
-        this._department = newDepartment;
-    }
-
-    getContent() {
-        return `<div class="user">${this.firstName} ${this.lastName}: зп ${this._salary} р; ${this._department};</div>`;
-    }
-
 }
